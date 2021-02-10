99542b30b5247339afc3030d7d701c9e0610f8e0
Amoghmule <amoghmule13@gmail.com>
2021-02-10 15:38:31 +0530

    initial commit

---
+++ src/main/java/com/rail/RailEnquiryAppApplication.java
@@ -0,0 +1,13 @@
+package com.rail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailEnquiryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailEnquiryAppApplication.class, args);
+	}
+
+}
