0a2bbd701918883edfbf3891c0056ba9d77b8c50
Amoghmule <amoghmule13@gmail.com>
2021-02-17 18:25:31 +0530

    added exception handling

--- src/main/java/com/rail/RailEnquiryAppApplication.java
+++ src/main/java/com/rail/RailEnquiryAppApplication.java
@@ -2,8 +2,10 @@ package com.rail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:ValidationMessages.properties")
 public class RailEnquiryAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rail/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/rail/exception/ExceptionControllerAdvice.java
@@ -1,7 +1,15 @@
 package com.rail.exception;
 
+
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,9 +25,42 @@ public class ExceptionControllerAdvice {
 		error.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
 		error.setMessage(ex.getMessage());
 		
+		ex.printStackTrace();
 		
 		
-		return ResponseEntity.ok(error);
+		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ErrorMessage> exceptionHandler2(BusinessException ex){
+		
+		ErrorMessage error=new ErrorMessage();
+	     error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+	     error.setMessage(ex.getMessage());
+	     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorMessage> handleValidationExceptions(MethodArgumentNotValidException ex){
+		
+		ErrorMessage error=new ErrorMessage();
+	     error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+	     error.setMessage(ex.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", ")));
+		
+		
+	     return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		}
 
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorMessage> handleConstraintViolationException(ConstraintViolationException ex){
+		ErrorMessage error=new ErrorMessage();
+	     error.setErrorCode(HttpStatus.BAD_REQUEST.value());
+	     error.setMessage(ex.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", ")));
+		
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
 }
