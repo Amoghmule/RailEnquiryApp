c992370622239ed9fb8f64d339d57508d02e7f95
Amoghmule <amoghmule13@gmail.com>
2021-06-20 16:22:39 +0530

    Added actuator and configured app for external tomcat deployment

--- src/main/java/com/rail/RailEnquiryAppApplication.java
+++ src/main/java/com/rail/RailEnquiryAppApplication.java
@@ -2,11 +2,12 @@ package com.rail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:ValidationMessages.properties")
-public class RailEnquiryAppApplication {
+public class RailEnquiryAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RailEnquiryAppApplication.class, args);
